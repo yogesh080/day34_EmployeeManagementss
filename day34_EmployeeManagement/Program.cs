77b8e1c51ef9f691f4115e51ecfc148754196de3
Yogesh <yogeshbietjhs@gmail.com>
2022-07-27 01:32:37 +0530

    incial commit

---
+++ day34_EmployeeManagement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace day34_EmployeeManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
