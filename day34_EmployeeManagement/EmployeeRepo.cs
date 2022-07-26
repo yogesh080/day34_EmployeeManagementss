39e8ff31d59dd689cb00f8613ff3bc187f5fe5a1
Yogesh <yogeshbietjhs@gmail.com>
2022-07-27 02:02:52 +0530

    data recieved

--- day34_EmployeeManagement/EmployeeRepo.cs
+++ day34_EmployeeManagement/EmployeeRepo.cs
@@ -16,7 +16,59 @@ namespace day34_EmployeeManagement
         List<SalaryDetailModel> employeeDetailsList = new List<SalaryDetailModel>();
         public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog =CompanyDB; Integrated Security = True;";
         SqlConnection connection = new SqlConnection(connectionString);
+        public void GetAllemployee()
+        {
+
+            try
+            {
+                using (this.connection)
+                {
+                    string query = "select * from Employeedetail";
+                    SqlCommand sqlCommand = new SqlCommand(query, this.connection);
+                    this.connection.Open();
+                    SqlDataReader dr = sqlCommand.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            SalaryDetailModel employeeModel = new SalaryDetailModel();
+                            employeeModel.EmployeeID = dr.GetInt32(0);
+                            employeeModel.EmployeeName = dr.GetString(1);
+                            employeeModel.Department = dr.GetString(2);
+                            employeeModel.Month = dr.GetString(3);
+                            employeeModel.EmployeeSalary = dr.GetInt32(4);
+                            employeeModel.SalaryID = dr.GetInt32(5);
+
+
+
+                            //display retrieved record
+                            employeeDetailsList.Add(employeeModel);
+
+
+                        }
+                        foreach (var employee in employeeDetailsList)
+                        {
+                            Console.WriteLine($"EmployeeID: {employee.EmployeeID}\nEmployeeName: {employee.EmployeeName}\nDepartment: {employee.Department}\nMonth: {employee.Month}\nEmployeeSalary: {employee.EmployeeSalary}\nSalaryID: {employee.SalaryID}");
+
+                        }
+
+                    }
+                    else
+                    {
+                        throw new Exception("No data found");
+                    }
+                    dr.Close();
+                    this.connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
 
         
+
     }
 }
